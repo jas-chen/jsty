7a5a266fe4ff5b4dcbfffc22a07ff94fed7b4fbd
Jas Chen <jas228@gmail.com>
2017-04-15 13:34:29 +0800

    Validate prop

--- src/style/prop.js
+++ src/style/prop.js
@@ -2,8 +2,8 @@ import cachedFn from '../utils/cachedFn';
 
 const createProp = prop => {
   if (process.env.NODE_ENV !== 'production') {
-    if ((/[A-Z]/g).test(prop)) {
-      throw new Error(`Prop should not contain uppercase characters: \`${prop}\`.`);
+    if ((/[^-a-z]/g).test(prop)) {
+      throw new Error(`Prop contains invalid characters: \`${prop}\`.`);
     }
   }
 
